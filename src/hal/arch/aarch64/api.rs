49e77ee3bf1f31e212c683b523d4616b0a35be89
周睿 <zrufo747@outlook.com>
2025-08-26 09:41:00 +0800

    fix: update bitflags and arm-gic-driver versions; refactor GIC driver handling in arch_api_impl

--- src/hal/arch/aarch64/api.rs
+++ src/hal/arch/aarch64/api.rs
@@ -1,5 +1,9 @@
 #[axvisor_api::api_mod_impl(axvisor_api::arch)]
 mod arch_api_impl {
+    use core::panic;
+
+    use axvisor_api::memory::virt_to_phys;
+
     extern fn hardware_inject_virtual_interrupt(irq: axvisor_api::vmm::InterruptVector) {
         crate::hal::arch::inject_interrupt(irq as _);
     }
@@ -17,18 +21,7 @@ mod arch_api_impl {
             // Use the GICv3 driver to read the typer register
             return gic.typer_raw();
         }
-
-        // use axstd::os::arceos::modules::axhal::irq::MyVgic;
-        // MyVgic::get_gicd().lock().get_typer()
-
-        // use memory_addr::pa;
-        // use std::os::arceos::modules::{axconfig, axhal};
-
-        unimplemented!();
-        // let typer_phys_addr = axconfig::devices::GICD_PADDR + 0x4;
-        // let typer_virt_addr = axhal::mem::phys_to_virt(pa!(typer_phys_addr));
-
-        // unsafe { core::ptr::read_volatile(typer_virt_addr.as_ptr_of::<u32>()) }
+        panic!("No GIC driver found");
     }
 
     extern fn read_vgicd_iidr() -> u32 {
@@ -47,21 +40,36 @@ mod arch_api_impl {
             return gic.iidr_raw();
         }
 
-        unimplemented!()
+        panic!("No GIC driver found");
     }
 
     extern fn get_host_gicd_base() -> memory_addr::PhysAddr {
-        // use std::os::arceos::api::config;
-        // unimplemented!();
-        // config::devices::GICD_PADDR.into()
-        0x800_0000.into()
+        let mut gic = rdrive::get_one::<rdrive::driver::Intc>()
+            .expect("Failed to get GIC driver")
+            .lock()
+            .unwrap();
+        if let Some(gic) = gic.typed_mut::<arm_gic_driver::v2::Gic>() {
+            let ptr: *mut u8 = gic.gicd_addr().as_ptr();
+            return virt_to_phys((ptr as usize).into());
+        }
+
+        if let Some(gic) = gic.typed_mut::<arm_gic_driver::v3::Gic>() {
+            let ptr: *mut u8 = gic.gicd_addr().as_ptr();
+            // Use the GICv3 driver to read the typer register
+            return virt_to_phys((ptr as usize).into());
+        }
+        panic!("No GIC driver found");
     }
 
     extern fn get_host_gicr_base() -> memory_addr::PhysAddr {
-        // use std::os::arceos::api::config;
-        // unimplemented!();
-        // config::devices::GICR_PADDR.into()
-        // TODO parse from dtb
-        0x80a_0000.into()
+        let mut gic = rdrive::get_one::<rdrive::driver::Intc>()
+            .expect("Failed to get GIC driver")
+            .lock()
+            .unwrap();
+        if let Some(gic) = gic.typed_mut::<arm_gic_driver::v3::Gic>() {
+            let ptr: *mut u8 = gic.gicr_addr().as_ptr();
+            return virt_to_phys((ptr as usize).into());
+        }
+        panic!("No GICv3 driver found");
     }
 }
